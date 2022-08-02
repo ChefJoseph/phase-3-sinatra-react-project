524852241da3cf08a6c4dcb896c729afe11faa7f
Joseph Hirotsu <jhirotsu@gmail.com>
2022-08-02 15:44:31 -0400

    application control update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,10 +13,33 @@ class ApplicationController < Sinatra::Base
     {message: "Hello to my users!"}.to_json
   end
 
+
   get "/transactions" do
     {message: "Hello to my transactions!"}.to_json
     transactions_all = Transaction.all 
     transactions_all.to_json
+  end
+
+  get "/transactions/:id" do
+	begin
+		found_transaction = Transaction.find(params[:id])
+		found_transaction.to_json
+	end
+end
+
+  post "/transactions" do
+    create_transactions = Transaction.create(params)
+    create_transactions.to_json
+  end
+ 
+  get "/transaction_sender" do
+    transaction_sender = Transaction.first.sender
+    transaction_sender.to_json
+  end
+
+  get "/transaction_receiver" do
+    transaction_receiver = Transaction.first.receiver 
+    transaction_receiver.to_json
   end
 
 end
